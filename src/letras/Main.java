9ce7f8dc660f9d75b816640f88a61ff4d31c86de
PabloL0pez <pablo.3895@outlook.com>
2017-10-16 11:42:33 -0300

    Oops

--- src/letras/Main.java
+++ src/letras/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			//generarCasoFatiga();
+			// generarCasoFatiga();
 			LetrasExtremas extremas = new LetrasExtremas("extremas.in");
 			extremas.resolver();
 		} catch (IOException e) {
